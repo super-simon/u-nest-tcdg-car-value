ef542826ac98668ace2820ed7ac2947d84544d87
Oleksandr Stetsiuk <oleksandr.v.stetsiuk@gmail.com>
2023-08-04 19:55:24 +0300

    signup

---
+++ src/users/auth.service.ts
@@ -0,0 +1,30 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { scrypt as _scrypt, randomBytes } from 'crypto';
+import { promisify } from 'util';
+import { UsersService } from './users.service';
+
+const scrypt = promisify(_scrypt);
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    const users = await this.usersService.find(email);
+    if (users.length) {
+      throw new BadRequestException('Email in use');
+    }
+
+    const salt = randomBytes(8).toString('hex');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    const result = salt + '.' + hash.toString('hex');
+
+    const user = await this.usersService.create(email, result);
+
+    return user;
+  }
+
+  // signin() {}
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,6 +10,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { AuthService } from './auth.service';
 import { CreateUserDTO } from './dtos/create-user.dto';
 import { UpdateUserDTO } from './dtos/update-user.dto';
 import { UserDto } from './dtos/user.dto';
@@ -18,11 +19,14 @@ import { UsersService } from './users.service';
 @Controller('auth')
 @Serialize(UserDto)
 export class UsersController {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+  ) {}
 
   @Post('/signup')
   createUser(@Body() body: CreateUserDTO) {
-    this.usersService.create(body.email, body.password);
+    this.authService.signup(body.email, body.password);
   }
 
   @Get('/:id')
